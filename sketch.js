082bff0af11f244fab37e1696062e20faa2da382
beomso0 <beomsookim98@naver.com>
2021-05-21 15:34:05 +0900

    modified

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ let dodge;
 let targetImg;
 let stage = 0;
 let myFont;
-let peach;
+let peace;
+let retire;
 
 function preload() {
   for(let i=0; i<5; i++) {
@@ -16,7 +17,8 @@ function preload() {
   img = loadImage("./assets/shiba_ani.gif");
   targetImg = loadImage("./assets/target_dodge.gif");
   myFont = loadFont("./assets/rocket.ttf");
-  peace = loadImage("./assets/peace_shiba.jpg");
+  peace = loadImage("./assets/peace_shiba2.jpg");
+  retire = loadImage("./assets/dev_shiba.gif");
 }
 
 function setup() {
@@ -49,6 +51,9 @@ function draw() {
     case 2:      
       image(peace, 0,0,width, height)
       break;
+    
+    case 3:      
+      image(retire, 0,0,width, height)
   }
     
 }
@@ -61,6 +66,14 @@ function mousePressed() {
   targetDodge.isGotten();
 }
 
+function keyPressed() {
+  if(key == 'r') {
+    stage =3;
+  } else if (key == 'p') {
+    stage =2;
+  }
+}
+
 function gameReady() {
   push();
   background(115, 115, 115,150);
@@ -68,18 +81,10 @@ function gameReady() {
   fill(255);
   textFont(myFont);
   textSize(50);
-  text("코인 도지를 잡아 탈출하세요", width/2,height/2-50);
+  text("KEY 도지를 잡아 탈출하세요", width/2,height/2-50);
   textSize(30);
-  text("미련이 남아 자꾸 클릭을 한다면, 탈출이 점점 더 어려워집니다.", width/2,height/2+70);
+  text("시간이 지날수록, 도지를 자꾸 클릭할수록 탈출이 점점 어려워집니다.", width/2,height/2+70);
+  text("탈출을 포기하시려면 R키를 누르세요.", width/2,height/2+120);
   textSize(15);
   pop();
 }
-
-
-// class Stick {
-//   constructor() {
-//     this.length = 100;
-
-//   }
-// }
-
